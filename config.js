275c42514068ee9d01a028875f0b4066b05089ce
fatimatabaknt <fatimatabaknt@gmail.com>
2026-06-05 15:41:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 212721422388;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
+  ['212721422388', 'Radwan Tawfiq', true],
   ['', 'Owner 2', true],
 ];
 
